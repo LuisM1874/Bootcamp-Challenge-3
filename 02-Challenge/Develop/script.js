a6361189d87b7225f7ef8dae0800ed4a48ac2a9e
Luis Moore <luismoore@Luiss-MacBook-Air.local>
2023-10-19 14:55:19 +0100

    added-JS

---
+++ 02-Challenge/Develop/script.js
@@ -0,0 +1,53 @@
+// Generate Button Code
+var generateBtn = document.querySelector("#generate");
+
+// Password Parameters
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers = "1234567890"
+var specials = "!@£$%^&*?~/"
+
+function generatePassword(){
+  var letters = ""
+  var numSpecial = ""
+
+  // This will pop up to explain to user how to use the function  
+  var passLength = prompt ("Pick a length for your password between 6-20 characters. Please type the length of password you would like");
+ 
+  //This will pop up when the user fails to select the correct criteria for their password.
+  if(passLength < 6 || passLength > 128 || isNaN(passLength)) {
+    alert ("The password must be between 6 and 20 characters , Please enter a value between these two values.");
+    return;
+
+//Asks user their parameters of their desired password
+  } else {
+  var lower = confirm("Do you want your password to have lowercase letters?");
+    if (lower) {characters += lowercase;}
+    var upper = confirm("Do you want your password to contain uppercase characters?");
+    if (upper) {characters += uppercase;}
+    var number = confirm("Do you want your password to contain numbers?");
+    if (number) {characters += numbers;}
+    var special = confirm("Do you want special characters in your password?");
+    if (special) {characters += specials;}
+
+  }
+
+  for (i=0 ; i < passLength; i++) {
+
+    randomPass += characters[Math.floor(Math.random()* characters.length)]
+  }
+  return(randomPass);
+}
+  var passwordText = document.querySelector("#password");
+
+  function writePassword (randomPass) {
+    if (passwordText.length === 0) {
+    return ;
+    }
+    passwordText.value = randomPass;
+  }
+
+  generateBtn.addEventListener("click", function () {writePassword(generatePassword())});
+
+
+  
